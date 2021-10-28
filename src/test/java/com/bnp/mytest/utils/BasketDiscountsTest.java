7e0778f6bc0e375c2c08e50723d4fc4e1eb6e359
ibtti <ibtibouallegue@gmail.com>
2021-10-28 14:38:39 +0200

    preparing the tests of the basket discounts

--- src/test/java/com/bnp/mytest/utils/BasketDiscountsTest.java
+++ src/test/java/com/bnp/mytest/utils/BasketDiscountsTest.java
@@ -39,4 +39,122 @@ public class BasketDiscountsTest {
     }
 
 
+    @Test
+    public void twoCopiesOfTheSameBook(){
+        //Given
+        Book CleanCode = Catalog.GivenACleanCodeBook();
+        //When
+        basket.add(CleanCode);
+        basket.add(CleanCode);
+        //Then
+        assertThat(basket.getTotalPrice(), is(100.0));
+
+    }
+
+    @Test
+    public void twoDifferentBooks(){
+        //Given
+        Book cleanCode = Catalog.GivenACleanCodeBook();
+        Book cleanArchitecture = Catalog.GivenACleanArchitectureBook();
+        //When
+        basket.add(cleanCode);
+        basket.add(cleanArchitecture);
+        //Then
+        assertThat(basket.getTotalPrice(), is(95.0));
+
+    }
+
+    @Test
+    public void threeDifferentCopies(){
+        //Given
+        Book cleanCode = Catalog.GivenACleanCodeBook();
+        Book cleanArchitecture = Catalog.GivenACleanArchitectureBook();
+        Book testDrivenDevelopmentByExample = Catalog.GivenATestDrivenDevelopmentByExampleBook();
+        //When
+        basket.add(cleanCode);
+        basket.add(cleanArchitecture);
+        basket.add(testDrivenDevelopmentByExample);
+        //Then
+        assertThat(basket.getTotalPrice(), is(135.0));
+
+    }
+
+    @Test
+    public void fourDifferentCopies(){
+        //Given
+
+        Book cleanCode = Catalog.GivenACleanCodeBook();
+        Book cleanArchitecture = Catalog.GivenACleanArchitectureBook();
+        Book testDrivenDevelopmentByExample = Catalog.GivenATestDrivenDevelopmentByExampleBook();
+        Book workingEffectivelyWithLegacyCode = Catalog.GivenAWorkingEffectivelyWithLegacyCodeBook();
+        //When
+        basket.add(cleanCode);
+        basket.add(cleanArchitecture);
+        basket.add(testDrivenDevelopmentByExample);
+        basket.add(workingEffectivelyWithLegacyCode);
+        //Then
+        assertThat(basket.getTotalPrice(), is(160.0));
+
+    }
+
+    @Test
+    public void fiveDifferentCopies(){
+        //Given
+
+        Book cleanCode = Catalog.GivenACleanCodeBook();
+        Book cleanArchitecture = Catalog.GivenACleanArchitectureBook();
+        Book testDrivenDevelopmentByExample = Catalog.GivenATestDrivenDevelopmentByExampleBook();
+        Book workingEffectivelyWithLegacyCode = Catalog.GivenAWorkingEffectivelyWithLegacyCodeBook();
+        Book theCleanCoder = Catalog.GivenATheCleanCoderBook();
+        //When
+        basket.add(cleanCode);
+        basket.add(cleanArchitecture);
+        basket.add(testDrivenDevelopmentByExample);
+        basket.add(workingEffectivelyWithLegacyCode);
+        basket.add(theCleanCoder);
+        //Then
+        assertThat(basket.getTotalPrice(), is(187.50));
+    }
+
+    @Test
+    public void fourCopiesAndThreeAreDifferent(){
+        //Given
+        Book cleanCode = Catalog.GivenACleanCodeBook();
+        Book cleanCodeII = Catalog.GivenACleanCodeBook();
+        Book cleanArchitecture = Catalog.GivenACleanArchitectureBook();
+        Book testDrivenDevelopmentByExample = Catalog.GivenATestDrivenDevelopmentByExampleBook();
+        //When
+        basket.add(cleanCode);
+        basket.add(cleanCodeII);
+        basket.add(cleanArchitecture);
+        basket.add(testDrivenDevelopmentByExample);
+        //Then
+        assertThat(basket.getTotalPrice(), is(185.00));
+
+    }
+
+    @Test
+    public void twoSetOfFourDifferentCopies(){
+        //Given
+        Book cleanCode = Catalog.GivenACleanCodeBook();
+        Book cleanCodeII = Catalog.GivenACleanCodeBook();
+        Book cleanArchitecture = Catalog.GivenACleanArchitectureBook();
+        Book cleanArchitectureII = Catalog.GivenACleanArchitectureBook();
+        Book testDrivenDevelopmentByExample = Catalog.GivenATestDrivenDevelopmentByExampleBook();
+        Book testDrivenDevelopmentByExampleII = Catalog.GivenATestDrivenDevelopmentByExampleBook();
+        Book workingEffectivelyWithLegacyCode = Catalog.GivenAWorkingEffectivelyWithLegacyCodeBook();
+        Book theCleanCoder = Catalog.GivenATheCleanCoderBook();
+        //When
+        basket.add(cleanCode);
+        basket.add(cleanCodeII);
+        basket.add(cleanArchitecture);
+        basket.add(cleanArchitectureII);
+        basket.add(testDrivenDevelopmentByExample);
+        basket.add(testDrivenDevelopmentByExampleII);
+        basket.add(workingEffectivelyWithLegacyCode);
+        basket.add(theCleanCoder);
+        //Then
+        assertThat(basket.getTotalPrice(), is(320.00));
+
+    }
 }
